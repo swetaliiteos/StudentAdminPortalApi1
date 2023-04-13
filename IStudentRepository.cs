0d815cf0eb633902a822e8cec8c319507728f738
swetaliiteos <129953852+swetaliiteos@users.noreply.github.com>
2023-04-13 13:29:08 +0530

    Add files via upload

---
+++ IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using StudentAdminPortalAPI.Datamodels;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortalAPI.Repositories
+{
+    public interface IStudentRepository
+    {
+       Task<List<student>>GetStudentsAsync();
+    }
+}
