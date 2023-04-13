a7eccab20e2529764759c997007b8a013eaf32fe
swetaliiteos <129953852+swetaliiteos@users.noreply.github.com>
2023-04-13 13:28:21 +0530

    Add files via upload

---
+++ AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using StudentAdminPortalAPI.DomainModels;
+using DataModels=StudentAdminPortalAPI.Datamodels;
+
+namespace StudentAdminPortalAPI.Profiles
+{
+    public class AutoMapperProfiles:Profile
+    {
+       public AutoMapperProfiles()
+        {
+            CreateMap<DataModels.student, Student>()
+           .ReverseMap();
+            CreateMap<DataModels.Gender, Gender>()
+           .ReverseMap();
+            CreateMap<DataModels.Address, Address>()
+           .ReverseMap();
+        }
+    }
+}
