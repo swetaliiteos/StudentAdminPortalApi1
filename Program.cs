fc4cce5c8b152932494f7ce2ca91e0da6c7a944a
Swetali <swetali@iteos.in>
2023-05-17 18:03:27 +0530

    done

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors((options) =>
+{
+    options.AddPolicy("angularApplication", (builder) =>
+    {
+       
+        builder.WithOrigins("http://localhost:4200")
+                           .AllowAnyHeader()
+                            .AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -25,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("angularApplication");
+//app.UseRouting();
 
 app.UseAuthorization();
 
